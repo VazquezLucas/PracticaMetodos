7dfdab381a15a454e57ed5b5df7d80bf02e78c98
Lucas Vazquez <vazquezlucasmatias2001@gmail.com>
2023-02-16 13:28:58 -0300

    15/02/2023
    Primeros pasos con metodos

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace Practica_Metodos
     {
         static void Main(string[] args)
         {
+            //mensajeEnPantalla();
+            //Console.WriteLine("Mensaje desde el main");
 
+            Console.WriteLine(sumaNumeros(7,9));
+            
+        }
+        /*
+        //creacion de mi propio metodo
+
+        static void mensajeEnPantalla()
+        {
+
+            Console.WriteLine("Mensaje desde el método mensajeEnPantalla");
+
+
+        }
+        
+        //Método que suma dos numeros
+
+        static void sumaNumeros(int num1, int num2)
+        {
+            Console.WriteLine($"La suma de los números es: {num1+num2}");
+        }
+        */
+
+        //Métodos con return
+
+        static int sumaNumeros(int num1, int num2)
+        {
+            return num1 + num2;
         }
     }
 }
